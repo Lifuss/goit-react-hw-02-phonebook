44c0ba23566a408a33a4991a24604691879f3dd8
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-09-11 19:26:49 +0300

    task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,75 @@
 import React, { Component } from 'react';
-import Section from './Section/Section';
-import {
-  countPositiveFeedbackPercentage,
-  countTotalFeedback,
-} from './Helpers/countFeedback';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import getFilteredData from './Helpers/getFilteredData';
+import { StyledDiv } from './App.styled';
 
 export class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  HandleButton = e => {
-    const { value } = e.target;
-    this.setState(prev => ({ [value]: prev[value] + 1 }));
+  // test htmlFor *_*
+  nameInputId = crypto.randomUUID();
+  phoneInputId = crypto.randomUUID();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = e.target.elements;
+
+    if (this.state.contacts.find(user => user.name === name.value)) {
+      alert(`${name.value} is already in contacts`);
+      return;
+    }
+
+    this.setState(
+      prev => ({
+        contacts: [
+          ...prev.contacts,
+          {
+            name: name.value,
+            number: number.value,
+            id: crypto.randomUUID(),
+          },
+        ],
+      }),
+      () => {
+        e.target.reset();
+      }
+    );
+  };
+  handleInput = e => {
+    this.setState({ filter: e.target.value });
+  };
+  handleDelete = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(el => el.id !== id),
+    }));
   };
 
   render() {
-    const { good, neutral, bad } = this.state;
-    const total = countTotalFeedback(this.state);
-    const positivePercentage = countPositiveFeedbackPercentage(good, total);
-
+    const { contacts, filter } = this.state;
+    const filteredData = getFilteredData(contacts, filter);
     return (
-      <Section
-        title="Please leave feedback"
-        good={good}
-        bad={bad}
-        neutral={neutral}
-        total={total}
-        positivePercentage={positivePercentage}
-        HandleButton={this.HandleButton}
-      ></Section>
+      <StyledDiv>
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.handleSubmit}
+          phoneInputId={this.phoneInputId}
+          nameInputId={this.nameInputId}
+        />
+
+        <h2>Contacts</h2>
+        <Filter onChange={this.handleInput} />
+        <ContactList contacts={filteredData} onDelete={this.handleDelete} />
+      </StyledDiv>
     );
   }
 }
--- src/components/Notification/Notification.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledNotification = styled.p`
-  font-size: 24px;
-  font-weight: 400;
-`;
